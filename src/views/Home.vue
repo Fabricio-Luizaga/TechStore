85200a9242272ce43a48237d33e947833613dc24
Fabricio-Luizaga <fabricio.luizagablazz@intjem.edu.bo>
2026-07-27 10:43:01 -0400

    Agregar workflow para GitHub Pages

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,30 @@
 <template>
-  <div class="p-10 text-center">
-    <h1 class="text-5xl font-bold text-red-600">
-      HOME FUNCIONANDO
-    </h1>
-  </div>
+
+<div>
+
+<HeroSection/>
+
+<FeaturedCategories/>
+
+<FeaturedProducts/>
+
+<WhyChooseUs/>
+
+<Testimonials/>
+
+<Newsletter/>
+
+</div>
+
 </template>
 
 <script setup>
+
+import HeroSection from '../components/HeroSection.vue'
+import FeaturedCategories from '../components/FeaturedCategories.vue'
+import FeaturedProducts from '../components/FeaturedProducts.vue'
+import WhyChooseUs from '../components/WhyChooseUs.vue'
+import Testimonials from '../components/Testimonials.vue'
+import Newsletter from '../components/Newsletter.vue'
+
 </script>
